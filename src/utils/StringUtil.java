dd2881577a354512c1cf034a2c9cc167ca39b611
SafiyeM <122940752+SafiyeM@users.noreply.github.com>
2023-05-12 22:26:48 +0200

    added StringUtil.java

---
+++ src/utils/StringUtil.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class StringUtil {
+
+    // This is String Util
+
+    public void util(){
+
+        System.out.println("Hi Cigdem, this method is for you, enjoy :)");
+    }
+
+}
